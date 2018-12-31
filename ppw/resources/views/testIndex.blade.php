cab7370c38c69e60cad1fe475e9af57027db0df9
Edoardo <iweek16@gmail.com>
2018-12-31 17:32:38 +0100

    Rinominato il file

---
+++ ppw/resources/views/testIndex.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Lista dei Test</title>
+
+
+    </head>
+    <body>
+        <ul>
+            <li><a href="testForm">Test Form</a></li>
+            <li><a href="testAjax">Test Ajax</a></li>
+            <li><a href="testFile">Test File</a></li>
+        </ul>
+    </body>
+</html>
