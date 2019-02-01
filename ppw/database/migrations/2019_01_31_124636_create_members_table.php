66bd230435f40fb80b01df00c01d16059ed471f5
Edoardo <iweek16@gmail.com>
2019-02-01 09:00:21 +0100

    Test join tabelle

---
+++ ppw/database/migrations/2019_01_31_124636_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('user_id')->unsigned();
+            $table->rememberToken();
+            $table->timestamps();
+
+            Schema::table('members', function (Blueprint $table) {
+
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
