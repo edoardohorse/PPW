ef0ba2c43e4bf3f19452242ba5816d23db66b586
Edoardo <iweek16@gmail.com>
2019-02-09 09:41:03 +0100

    Salvati offline gli asset bootstrap (js/css)
    - Risolto il problema del js di bootstrap, scambiando l'ordine di importazione con jquery

--- ppw/resources/views/layout/master_lib.blade.php
+++ ppw/resources/views/layout/master_lib.blade.php
@@ -2,7 +2,7 @@
 
     CSS
 --}}
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/bootstrap.css')  }}">
 <link rel="stylesheet" href= "{{ asset('css/jquery.steps.css') }}" />
 
 @yield('lib_css')
@@ -12,8 +12,8 @@
 {{--
     SCRIPT
 --}}
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <script src="{{ asset('js/jquery.js')  }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}" ></script>
 @yield('lib_script')
 
 
