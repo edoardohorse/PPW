371231fe8276564b7223b3ca6a5b58ff5045ce9c
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2019-02-13 11:58:53 +0100

    Aggiunto il calendario

--- ppw/app/Http/Controllers/CalendarController.php
+++ ppw/app/Http/Controllers/CalendarController.php
@@ -9,22 +9,55 @@ use MaddHatter\LaravelFullcalendar\Facades\Calendar;
 
 class CalendarController extends Controller
 {
+    private static $DAYS = ['Lunedi','Martedi','Mercoledi','Giovedi','Venerdi','Sabato','Domenica'];
+
+
+
+
+    private function  getStartAndEndDate($week, $year) {
+        $dto = new \DateTime();
+        $dto->setISODate($year, $week);
+        $ret['week_start'] = $dto->format('d');
+        $dto->modify('+6 days');
+        $ret['week_end'] = $dto->format('d');
+        return $ret;
+    }
+
+    private function getArrayByWeek(){
+        $week_array = $this->getStartAndEndDate(date('W'),date('Y'));
+
+        $days = [];
+        $first = $week_array['week_start'];
+        $end = $week_array['week_end'];
+
+        for($i=0;$first<=$end;$first++,$i++){
+            $tmp = $first;
+            $tmp .='-'.(string) date('m');
+            $tmp.='-'.(string) date('Y');
+            $days[CalendarController::$DAYS[$i]] = $tmp;
+        }
+        return $days;
+    }
+
+
     public function calendar(){
 
         $events = [];
-        $data = DB::select("SELECT sh.ora_inizio, sh.ora_fine, c.nome_corso
+        $data = DB::select("SELECT sh.ora_inizio, sh.ora_fine, c.nome_corso, sh.giorno
 FROM schedulings as sh, courses as c, scheduling_course as shc
 WHERE c.id = shc.course_id
 AND   shc.scheduling_id = sh.id");
 
+    $days = $this->getArrayByWeek();
 
+//    dd($days);
 
-            foreach ($data as $key => $value) {
+    foreach ($data as $key => $value) {
                 $events[] = Calendar::event(
                   $value->nome_corso,
                 false,
-                new \DateTime($value->ora_inizio),
-                new \DateTime($value->ora_fine),
+                new \DateTime($days[$value->giorno].' '.$value->ora_inizio),
+                new \DateTime($days[$value->giorno].' '.$value->ora_fine),
                 null,
                 // Add color and link on event
                 [
