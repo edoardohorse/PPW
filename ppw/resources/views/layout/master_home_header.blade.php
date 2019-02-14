796148267fb7348f6e1e49d20f8a863aee468233
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2019-02-14 10:05:38 +0100

    Aggiunti font large e spostati le shortcut

--- ppw/resources/views/layout/master_home_header.blade.php
+++ ppw/resources/views/layout/master_home_header.blade.php
@@ -33,13 +33,32 @@
 
 
             <a href="{{route('logout')}}" id="btn-logout" class="btn btn-info btn-sm">Esci</a>
+
+
         </div>
 
         {{--<div class="row float-right">--}}
             {{--<a href="/settings_account" id="btn-account" class="btn btn-light btn-lg">Account Personale</a>--}}
         {{--</div>--}}
 
-    </div>
 
+
+        <div>
+            <i id="font-large" style="background-image: url(' {{ asset('img/font.png')  }} ')" onclick="changeFont()" title="Ingrandisci/Diminuisci font"></i>
+            <script>
+                function changeFont(){
+                    if($('body').hasClass('font-large')){
+                        $('body').removeClass('font-large')
+                    }
+                    else{
+                        $('body').addClass('font-large')
+                    }
+                }
+            </script>
+        </div>
+
+        @include('layout.master_home_shortcuts')
+
+    </div>
 
 </header>
