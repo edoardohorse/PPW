2fd4576b7f6801b272801590aac3b6530cdd9d40
Edoardo <iweek16@gmail.com>
2019-02-15 21:54:45 +0100

    Fix query, id sbagliato selezionato

--- ppw/app/Http/Controllers/StaffExternalController.php
+++ ppw/app/Http/Controllers/StaffExternalController.php
@@ -295,7 +295,7 @@ class StaffExternalController extends Controller
 //        AND co.esterno = 1
 //    ORDER BY m.id ASC');
 
-        return DB::select('SELECT DISTINCT co.id,u.nome,u.cognome,m.cod_fiscale,u.data_nascita,m.scadenza_ass,m.scadenza_cert_med
+        return DB::select('SELECT DISTINCT m.id,u.nome,u.cognome,m.cod_fiscale,u.data_nascita,m.scadenza_ass,m.scadenza_cert_med
 FROM users u,members m,collaborators co
 WHERE m.id = u.member_id AND u.id = co.user_id 
 AND co.esterno = 1   
