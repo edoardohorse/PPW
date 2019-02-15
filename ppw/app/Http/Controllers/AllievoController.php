f186b28fdf10114da2f8cf118b88437c5d548c6f
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2019-02-15 14:17:45 +0100

    Modificata query allievo

--- ppw/app/Http/Controllers/AllievoController.php
+++ ppw/app/Http/Controllers/AllievoController.php
@@ -339,10 +339,9 @@ class AllievoController extends Controller
     }
 
     private function fetchAll(){
-        return DB::select('SELECT DISTINCT m.id,u.nome,u.cognome,m.cod_fiscale,u.data_nascita,c.scadenza_tesseramento,m.scadenza_ass,m.scadenza_cert_med 
-FROM users u,cards c,members m,collaborators co,teachers t 
-WHERE m.id = u.member_id AND u.id = co.user_id AND 
-co.id = t.collaborator_id AND t.stagista = 1 
- GROUP BY m.id ASC ');
+        return DB::select("SELECT DISTINCT m.id,u.nome,u.cognome,m.cod_fiscale,u.data_nascita,c.scadenza_tesseramento,m.scadenza_ass,m.scadenza_cert_med 
+FROM users u,cards c,members m 
+WHERE m.id = u.member_id AND u.tipo = 'allievo' 
+ GROUP BY m.id ASC ");
     }
 }
